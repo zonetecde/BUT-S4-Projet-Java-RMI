fc0909514fd6980a9d96a4e71376f255b3f7bc29
Wannnnnnner <erwann.brissinger8@etu.univ-lorraine.fr>
2026-06-04 15:45:27 +0200

    création d'une méthode pour découper une image en NxN zones en ayant leurs coordonnées et leur taille

--- LancerRaytracer.java
+++ LancerRaytracer.java
@@ -8,6 +8,46 @@ import raytracer.Image;
 public class LancerRaytracer {
 
     public static String aide = "Raytracer : synthèse d'image par lancé de rayons (https://en.wikipedia.org/wiki/Ray_tracing_(graphics))\n\nUsage : java LancerRaytracer [fichier-scène] [largeur] [hauteur]\n\tfichier-scène : la description de la scène (par défaut simple.txt)\n\tlargeur : largeur de l'image calculée (par défaut 512)\n\thauteur : hauteur de l'image calculée (par défaut 512)\n";
+    
+    /**
+     * Calcule les zones de découpe pour diviser une image en n x n carrés.
+     * 
+     * @param nbreDecoupes nombre de lignes/colonnes de découpe (ex: 2 pour 4 carrés, 3 pour 9 carrés)
+     * @param width largeur de l'image
+     * @param height hauteur de l'image
+     * @return tableau contenant pour chaque sous-tableau: [x, y, w, h]
+     */
+    public static int[][] calculerZones(int nbreDecoupes, int width, int height) {
+        // tableau qui va stocker les coordonnées et tailles de chaque zone : [x, y, w, h]
+        int[][] zones = new int[nbreDecoupes * nbreDecoupes][4];
+        
+        // calcul de la largeur et hauteur de chaque zone
+        int largeurZone = width / nbreDecoupes;
+        int hauteurZone = height / nbreDecoupes;
+        
+        // on parcourt les lignes et colonnes pour calculer les coordonnées de chaque zone
+        int index = 0;
+        for(int ligne = 0; ligne < nbreDecoupes; ligne++) {
+            for(int colonne = 0; colonne < nbreDecoupes; colonne++) {
+                int x = colonne * largeurZone;
+                int y = ligne * hauteurZone;
+                
+                // pour la dernière colonne/ligne, on ajuste la taille pour couvrir le reste
+                // de l'image au cas ou width ou height ne soit pas divisible par nbreDecoupes
+                int w = (colonne == nbreDecoupes - 1) ? (width - x) : largeurZone;
+                int h = (ligne == nbreDecoupes - 1) ? (height - y) : hauteurZone;
+                
+                // stock les coordonnées et tailles de la zone dans le tableau
+                zones[index][0] = x;
+                zones[index][1] = y;
+                zones[index][2] = w;
+                zones[index][3] = h;
+                index++;
+            }
+        }
+        
+        return zones;
+    }
      
     public static void main(String args[]){
 
@@ -16,60 +56,35 @@ public class LancerRaytracer {
 
         // largeur et hauteur par défaut de l'image à reconstruire
         int largeur = 512, hauteur = 512;
+
+        var zones = calculerZones(2, largeur, hauteur);
         
-        if(args.length > 0){
-            fichier_description = args[0];
-            if(args.length > 1){
-                largeur = Integer.parseInt(args[1]);
-                if(args.length > 2)
-                    hauteur = Integer.parseInt(args[2]);
-            }
-        }else{
-            System.out.println(aide);
-        }
         // création d'une fenêtre 
         Disp disp = new Disp("Raytracer", largeur, hauteur);
         
         // Initialisation d'une scène depuis le modèle 
         Scene scene = new Scene(fichier_description, largeur, hauteur);
 
-        // On découpe l'image en deux dimensions pour avoir 2x2 zones
-        int demiLargeur = largeur / 2;
-        int demiHauteur = hauteur / 2;
 
         // Chronométrage du temps de calcul
         Instant debut = Instant.now();
 
         System.out.println("Calcul de l'image :");
 
-        // Première zone : haut gauche
-        // Celle-ci commence à x = 0 et y = 0, et sa taille est de demiLargeur x demiHauteur
-        int x0 = 0;
-        int y0 = 0;
-        int l = demiLargeur;
-        int h = demiHauteur;
 
-        System.out.println(" - Zone haut gauche : coordonnées " + x0 + "," + y0
-                           + " taille " + l + "x" + h);
+        System.out.println(" - Zone haut gauche : coordonnées " + zones[0][0] + "," + zones[0][1]
+                           + " taille " + zones[0][2] + "x" + zones[0][3]);
 
         // Appel de la méthode de calcul pour cette zone
-        Image imageHautGauche = scene.compute(x0, y0, l, h);
-        disp.setImage(imageHautGauche, x0, y0);
+        Image imageHautGauche = scene.compute(zones[0][0], zones[0][1], zones[0][2], zones[0][3]);
+        disp.setImage(imageHautGauche, zones[0][0], zones[0][1]);
 
-        // Deuxième zone : bas droite
-        // Celle-ci commence à x = demiLargeur et y = demiHauteur, et sa taille est de (largeur - demiLargeur) x (hauteur - demiHauteur)
-        // Note : quand on a une taille PAIRE, demiLargeur x demiLargeur aurait été suffisant, mais quand on a une taille IMPAIRE, il faut faire attention à ne pas dépasser la taille totale de l'image du coup on a fait (largeur - demiLargeur) et (hauteur - demiHauteur)
-        x0 = demiLargeur;
-        y0 = demiHauteur;
-        l = largeur - demiLargeur;
-        h = hauteur - demiHauteur;
-
-        System.out.println(" - Zone bas droite : coordonnées " + x0 + "," + y0
-                           + " taille " + l + "x" + h);
+        System.out.println(" - Zone bas droite : coordonnées " + zones[3][0] + "," + zones[3][1]
+                           + " taille " + zones[3][2] + "x" + zones[3][3]);
 
         // Appel de la méthode de calcul pour cette zone
-        Image imageBasDroite = scene.compute(x0, y0, l, h);
-        disp.setImage(imageBasDroite, x0, y0);
+        Image imageBasDroite = scene.compute(zones[3][0], zones[3][1], zones[3][2], zones[3][3]);
+        disp.setImage(imageBasDroite, zones[3][0], zones[3][1]);
 
         Instant fin = Instant.now();
 
