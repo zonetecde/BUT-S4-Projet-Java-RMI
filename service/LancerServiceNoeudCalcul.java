9aa669c393eedc13959ee0845ad78374d5d2b4c5
zonetecde <zonetecde@gmail.com>
2026-06-08 09:29:00 +0200

    fix(rmi): inscription des noeuds via le distributeur

--- service/LancerServiceNoeudCalcul.java
+++ service/LancerServiceNoeudCalcul.java
@@ -1,23 +1,26 @@
 package service;
 
-import java.rmi.server.UnicastRemoteObject;
 import java.rmi.ConnectException;
-import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class LancerServiceNoeudCalcul {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         try {
-//Déclarer l'objet
+            // on commence par déclarer l'objet partagé
             NoeudCalcul o = new NoeudCalcul();
-//Transformer l'objet en service
+            // on le tranforme en service
             ServiceNoeudCalcul s = (ServiceNoeudCalcul) UnicastRemoteObject.exportObject(o, 0);
-//Récuperer l'annuaire
+            // recup l'annuaire
             Registry reg = LocateRegistry.getRegistry(1099);
-//Lier annuaire et service
-            reg.rebind("noeudCalcul", s);
+            // on recup ensuite le distributeur et on s'y enregistre
+            ServiceDistributeur distributeur = (ServiceDistributeur) reg.lookup("distributeur");
+            distributeur.enregistrerClient(s);
         } catch (ConnectException e) {
             System.out.println("L'annuaire n'est pas accessible ou pas lancé");
+        } catch (java.rmi.NotBoundException e) {
+            System.out.println("Le distributeur n'est pas disponible dans l'annuaire");
         } catch (Exception e) {
             System.out.println("Autre Exception :" + e);
         }
